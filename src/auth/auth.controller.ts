46b62e6f9423fdccf42ebc19c4cc280bea291924
sebasbit <sebastian.torres0426@gmail.com>
2025-07-13 12:09:00 -0500

    feat: implement user login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { AuthService } from './auth.service';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller()
 export class AuthController {
@@ -10,5 +11,10 @@ export class AuthController {
   @MessagePattern('auth.register_user')
   register(@Payload() registerUserDto: RegisterUserDto) {
     return this.authService.registerUser(registerUserDto);
+  }
+
+  @MessagePattern('auth.login_user')
+  login(@Payload() loginUserDto: LoginUserDto) {
+    return this.authService.loginUser(loginUserDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { RegisterUserDto } from './dto/register-user.dto';
 import { PrismaService } from 'src/prisma.service';
 import { createRpcException } from 'src/common/exceptions/create-rpc-exception';
 import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -31,6 +32,27 @@ export class AuthService {
       id: newUser.id,
       name: newUser.name,
       email: newUser.email,
+    };
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      throw createRpcException(
+        HttpStatus.BAD_REQUEST,
+        'Email or password is not valid',
+      );
+    }
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
     };
   }
 }
